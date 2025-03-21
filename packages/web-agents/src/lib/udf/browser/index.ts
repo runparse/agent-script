73bf2fec6e64e74ca599f3ac884055dd7c81e94a
William Wen <william@heyarctic.io>
2025-03-21 09:50:48 -0400

    chore: clean up tools
    Refs: NO-TICKET

--- packages/web-agents/src/lib/udf/browser/index.ts
+++ packages/web-agents/src/lib/udf/browser/index.ts
@@ -2,5 +2,4 @@ export * from './pageExtractDataUdf';
 export * from './pageClickUdf';
 export * from './pageNavigateUrlUdf';
 export * from './pageUdf';
-export * from './pageClosePopupsUdf';
 export * from './pageGoBack';
